a6d9ab60fa4ecea0952ede4a1561d6d32568e2f6
Ney Fonseca <ney.fonseca@fcamara.com.br>
2024-04-20 20:02:19 -0300

    fix: function all with output pub x509 error when folder have enc key

--- src/helpers/make-jwk.ts
+++ src/helpers/make-jwk.ts
@@ -1,14 +1,19 @@
-import { JWK, exportJWK, importPKCS8, importX509 } from 'jose'
+import { JWK, exportJWK, importPKCS8, importSPKI, importX509 } from 'jose'
 import { thumbprint } from './thumbprint'
 
 export const makeJwk = async (x509?: string, pkcs8?: string): Promise<JWK> => {
 	if (!x509) {
 		return null
 	}
-	const key = pkcs8 ? await importPKCS8(pkcs8, 'PS256') : await importX509(x509, 'PS256')
+	const isEnc = x509.includes('PUBLIC')
+
+	const key = pkcs8
+		? await importPKCS8(pkcs8, 'PS256')
+		: isEnc
+			? await importSPKI(x509, 'PS256')
+			: await importX509(x509, 'PS256')
 	const jwk = await exportJWK(key)
 
-	const isEnc = x509.includes('PUBLIC')
 	jwk.alg = isEnc ? 'RSA-OAEP' : 'PS256'
 	jwk.kid = thumbprint(x509, isEnc ? 'hex' : 'base64')
 	jwk.use = isEnc ? 'enc' : 'sig'
